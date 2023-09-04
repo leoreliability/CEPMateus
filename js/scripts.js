f24374874ec7b7d822efaeffcd62b7490f896937
Mateus Faustino <mateusfaustino2017@gmail.com>
2023-09-04 00:09:20 -0300

    fetch

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,15 @@
 window.addEventListener('load',()=>{
-    const $zipcodeButton = document.getElementById('app__zipcode__button')
-    $zipcodeButton.addEventListener('click',handleSubmit)
+    const button = document.getElementById('app__zipcode__button')
+    
+    button.addEventListener('click',handleSubmit)
 })
 
 function handleSubmit(e) {
     e.preventDefault()
+    const cepInput = document.getElementById('cep-input')
+    const cepValue = cepInput.value
     
+    pesquisarEndereco(cepValue)
 }
 
 // new XMLHttpRequest()
@@ -13,6 +17,51 @@ function handleSubmit(e) {
 // http.send()
 // http.onerror = () => {}
 // http.onload = () => {}
+// http.response
+
+function pesquisarEndereco(value){
+    showLoad()
+    return fetch('https://viacep.com.br/ws/'+value+'/json/')
+    .then((response)=>response.json())
+    .then(data => {
+        renderResponse('[data-cep]',data.cep)
+        renderResponse('[data-localidade]',data.localidade)
+        renderResponse('[data-logradouro]',data.logradouro)
+        renderResponse('[data-uf]',data.uf)
+        renderResponse('[data-bairro]',data.bairro)
+        renderResponse('[data-ddd]',data.ddd)
+        showResult()
+
+        if(data.erro){
+            showError()
+            return
+        }
+    }).catch((error)=>{
+        showError()
+        console.log(
+          "There has been a problem with your fetch operation: " + error.message,
+        );
+        
+    })
+}
+
+function showLoad(){
+    hideElementByid('adress')
+    hideElementByid('error')
+    showElementByid('load')
+}
+
+function showResult(){
+    hideElementByid('load')
+    hideElementByid('error')
+    showElementByid('adress')
+}
+
+function showError(){
+    hideElementByid('load')
+    hideElementByid('adress')
+    showElementByid('error')
+}
 
 function renderResponse(tagSelector, value){
     const $cep = document.querySelector(tagSelector)
@@ -30,6 +79,5 @@ function hideElementByid(id){
 }
 
 function handleError(){
-    hideElementByid('load')
-    showElementByid('error')
+    
 }
